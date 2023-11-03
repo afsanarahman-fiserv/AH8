8f9627bf1c6e5df3f9d167888bf5f3a451ab0447
Rahman <afsana.rahman@fiserv.com>
2023-11-03 13:16:29 -0400

    Inital solution

--- ECSource.java
+++ ECSource.java
@@ -1,29 +1,21 @@
-class Node<T> {
-  T val;
-  Node<T> next;
-  
-  public Node(T val) {
-    this.val = val;
-    this.next = null;
-  }
-}
-
 class ECSource {
   public static <T> T getNodeValue(Node<T> head, int index) {
-    // todo - note the return type, but don't overthink it
-    return;
+    for (int i = 0; i < index; i++) {
+      if(head == null) {
+        return null;
+      }
+      head = head.next;
+    }
+    return head.val;
   }
   
   public static void main(String[] args) {
     Node<String> node1 = new Node<>("banana");
     Node<String> node2 = new Node<>("mango");
     Node<String> node3 = new Node<>("kiwi");
-
     node1.next = node2;
     node2.next = node3;
-
     // banana -> mango -> kiwi
-
     System.out.println(ECSource.getNodeValue(node1, 1));
   }
 }
--- Source.java
+++ Source.java
@@ -10,7 +10,12 @@ class Node<T> {
   
   class Source {
     public static <T> boolean linkedListFind(Node<T> head, T target) {
-      // todo
+      while(head != null) {
+          if(head.val.equals(target)) {
+              return true;
+          }
+          head = head.next;
+      }
       return false;
     }
     
@@ -19,14 +24,14 @@ class Node<T> {
         Node<String> b = new Node<>("b");
         Node<String> c = new Node<>("c");
         Node<String> d = new Node<>("d");
-
         a.next = b;
         b.next = c;
         c.next = d;
-
         // a -> b -> c -> d
-
         System.out.println(Source.linkedListFind(a, "c"));
+
+        Node<Integer> node1 = new Node<>(42);
+        System.out.println(Source.linkedListFind(node1, 100));
         
     }
 
